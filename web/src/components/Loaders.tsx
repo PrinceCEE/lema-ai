d4398f0aa6af9e32ae60ca16b15ec376720e3709
Chimezie Edeh <princecee15@gmail.com>
2025-02-16 12:42:00 +0100

    feat: update Loader component background color for improved visibility

--- web/src/components/Loaders.tsx
+++ web/src/components/Loaders.tsx
@@ -11,21 +11,21 @@ export const Loader: FC<{ bg?: string; isSmall?: boolean }> = ({
         className={clsx(
           `${
             isSmall ? "w-[4.18px] h-[4.18px]" : "w-[11.6633px] h-[11.6633px]"
-          } ${bg ?? "bg-[#7f56d9]"}`
+          } ${bg ?? "bg-[#bdb1c6]"}`
         )}
       ></div>
       <div
         className={clsx(
           `${
             isSmall ? "w-[5.85px] h-[5.85px]" : "w-[13.3333px] h-[13.3333px]"
-          } ${bg ?? "bg-[#7f56d9]"}`
+          } ${bg ?? "bg-[#bdb1c6]"}`
         )}
       ></div>
       <div
         className={clsx(
           `${
             isSmall ? "w-[5.85px] h-[5.85px]" : "w-[13.3333px] h-[13.3333px]"
-          } ${bg ?? "bg-[#7f56d9]"}`
+          } ${bg ?? "bg-[#bdb1c6]"}`
         )}
       ></div>
     </div>
